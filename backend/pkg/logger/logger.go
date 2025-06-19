c129efd234022706800d2564f22172b3acd5edfb
doryngal <141703428+doryngal@users.noreply.github.com>
2025-06-20 02:45:01 +0500

    feat(logger): add slog-based logging module with Logger interface
    - Implemented Logger interface with Info, Error, Debug methods
    - Created stdLogger using slog with text handler and debug level
    - Logs output to stdout

--- backend/pkg/logger/logger.go
+++ backend/pkg/logger/logger.go
@@ -1,1 +1,37 @@
 package logger
+
+import (
+	"log/slog"
+	"os"
+)
+
+type Logger interface {
+	Info(msg string, keysAndValues ...interface{})
+	Error(msg string, keysAndValues ...interface{})
+	Debug(msg string, keysAndValues ...interface{})
+}
+
+type stdLogger struct {
+	base *slog.Logger
+}
+
+func New() Logger {
+	handler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	})
+	return &stdLogger{
+		base: slog.New(handler),
+	}
+}
+
+func (l *stdLogger) Info(msg string, keysAndValues ...interface{}) {
+	l.base.Info(msg, keysAndValues...)
+}
+
+func (l *stdLogger) Error(msg string, keysAndValues ...interface{}) {
+	l.base.Error(msg, keysAndValues...)
+}
+
+func (l *stdLogger) Debug(msg string, keysAndValues ...interface{}) {
+	l.base.Debug(msg, keysAndValues...)
+}
