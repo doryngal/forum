2ae6a48637f82a158c99be8554bd7f070659ffd8
doryngal <141703428+doryngal@users.noreply.github.com>
2025-06-20 02:06:49 +0500

    feat(database): add SQLite wrapper with open/close methods
    - Implemented SQLite struct to encapsulate *sql.DB
    - Added constructor NewSQLite and basic methods: Close and GetDB

--- backend/pkg/database/sqlite.go
+++ backend/pkg/database/sqlite.go
@@ -1,1 +1,23 @@
 package database
+
+import "database/sql"
+
+type SQLite struct {
+	db *sql.DB
+}
+
+func NewSQLite() (*SQLite, error) {
+	db, err := sql.Open("sqlite3", "./store.db")
+	if err != nil {
+		return nil, err
+	}
+	return &SQLite{db: db}, nil
+}
+
+func (s *SQLite) Close() error {
+	return s.db.Close()
+}
+
+func (s *SQLite) GetDB() *sql.DB {
+	return s.db
+}
