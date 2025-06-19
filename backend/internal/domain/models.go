efe672587303e93b2f20ebc6ecf379b01faba39d
doryngal <141703428+doryngal@users.noreply.github.com>
2025-06-19 20:16:28 +0500

    add: added model structures

--- backend/internal/domain/models.go
+++ backend/internal/domain/models.go
@@ -1,1 +1,19 @@
 package domain
+
+import "github.com/google/uuid"
+
+type Post struct {
+	ID uuid.UUID `json:"id"`
+}
+
+type User struct {
+	ID uuid.UUID `json:"id"`
+}
+
+type Comment struct {
+	ID uuid.UUID `json:"id"`
+}
+
+type Category struct {
+	ID uuid.UUID `json:"id"`
+}
