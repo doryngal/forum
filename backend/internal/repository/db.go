524e4540518edf1408fc7b63a0fa907b02ebd37d
doryngal <141703428+doryngal@users.noreply.github.com>
2025-06-23 21:16:47 +0500

    refactor(repo): centralize repository initialization in NewRepositories - Added Repositories struct to hold all domain repositories - Simplifies dependency injection and service wiring - Each repository now initialized through a single entry point

--- backend/internal/repository/db.go
+++ backend/internal/repository/db.go
@@ -1,1 +1,25 @@
 package repository
+
+import (
+	"database/sql"
+	category_repo "forum/internal/repository/sqlite/category"
+	comment_repo "forum/internal/repository/sqlite/comment"
+	post_repo "forum/internal/repository/sqlite/post"
+	user_repo "forum/internal/repository/sqlite/user"
+)
+
+type Repositories struct {
+	Category category_repo.Repository
+	Comment  comment_repo.Repository
+	Post     post_repo.Repository
+	User     user_repo.Repository
+}
+
+func NewRepositories(db *sql.DB) *Repositories {
+	return &Repositories{
+		Category: category_repo.New(db),
+		Comment:  comment_repo.New(db),
+		Post:     post_repo.New(db),
+		User:     user_repo.New(db),
+	}
+}
