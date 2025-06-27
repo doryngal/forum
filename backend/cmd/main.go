6e8c2dad65b847c6539630bbf6c6fbbacfdc55b9
doryngal <141703428+doryngal@users.noreply.github.com>
2025-06-27 16:37:52 +0500

    feat: add main application entry point with HTTP server startup

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,1 +1,22 @@
-package cmd
+package main
+
+import (
+	"forum/internal/config"
+	"forum/internal/router"
+	"log"
+	"net/http"
+)
+
+func main() {
+	cfg := config.LoadConfig()
+
+	app, err := router.NewApp(cfg)
+	if err != nil {
+		log.Fatal("failed to start app:", err)
+	}
+
+	log.Println("Server is running on http://localhost:" + cfg.Server.Port)
+	if err := http.ListenAndServe(":"+cfg.Server.Port, app); err != nil {
+		log.Fatal(err)
+	}
+}
