dd4da1ad96179757b46bd3014895dfd33d4706f5
doryngal <141703428+doryngal@users.noreply.github.com>
2025-06-28 00:55:47 +0500

    feat(service): integrate session service with validator into service layer

--- backend/internal/service/service.go
+++ backend/internal/service/service.go
@@ -3,19 +3,21 @@ package service
 import (
 	"forum/internal/repository"
 	category_service "forum/internal/service/category"
-	comment_service "forum/internal/service/comment"
-	post_service "forum/internal/service/post"
-	user_service "forum/internal/service/user"
-
 	category_validator "forum/internal/service/category/validator"
+	comment_service "forum/internal/service/comment"
 	comment_validator "forum/internal/service/comment/validator"
+	post_service "forum/internal/service/post"
 	post_validator "forum/internal/service/post/validator"
+	session_service "forum/internal/service/session"
+	session_validator "forum/internal/service/session/validator"
+	user_service "forum/internal/service/user"
 	user_validator "forum/internal/service/user/validator"
 )
 
 type Service struct {
 	Category category_service.Service
 	Comment  comment_service.Service
+	Session  session_service.Service
 	Post     post_service.Service
 	User     user_service.Service
 }
@@ -24,6 +26,7 @@ func NewServices(reps *repository.Repositories) *Service {
 	return &Service{
 		Category: category_service.New(reps.Category, reps.Post, category_validator.NewCategoryValidator()),
 		Comment:  comment_service.New(reps.Comment, reps.Post, reps.User, comment_validator.NewCommentValidator()),
+		Session:  session_service.New(reps.Session, session_validator.NewSessionValidator()),
 		Post:     post_service.New(reps.Post, reps.User, reps.Category, post_validator.NewPostValidator()),
 		User:     user_service.New(reps.User, user_validator.NewUserValidator()),
 	}
