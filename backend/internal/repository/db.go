225c9b15742af58e81d5bafb00c2158ad962bd1f
doryngal <141703428+doryngal@users.noreply.github.com>
2025-06-28 00:53:19 +0500

    chore(repository): add session repository to centralized Repositories struct

--- backend/internal/repository/db.go
+++ backend/internal/repository/db.go
@@ -5,12 +5,14 @@ import (
 	category_repo "forum/internal/repository/sqlite/category"
 	comment_repo "forum/internal/repository/sqlite/comment"
 	post_repo "forum/internal/repository/sqlite/post"
+	session_repo "forum/internal/repository/sqlite/session"
 	user_repo "forum/internal/repository/sqlite/user"
 )
 
 type Repositories struct {
 	Category category_repo.Repository
 	Comment  comment_repo.Repository
+	Session  session_repo.Repository
 	Post     post_repo.Repository
 	User     user_repo.Repository
 }
@@ -19,6 +21,7 @@ func NewRepositories(db *sql.DB) *Repositories {
 	return &Repositories{
 		Category: category_repo.New(db),
 		Comment:  comment_repo.New(db),
+		Session:  session_repo.New(db),
 		Post:     post_repo.New(db),
 		User:     user_repo.New(db),
 	}
