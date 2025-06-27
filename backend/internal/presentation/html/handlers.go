a7fe49f203c6b2c40e3594a9da52ffa03d0252ed
doryngal <141703428+doryngal@users.noreply.github.com>
2025-06-27 16:32:18 +0500

    refactor(html): extend TemplateHandlers with auth and post handlers

--- backend/internal/presentation/html/handlers.go
+++ backend/internal/presentation/html/handlers.go
@@ -1,16 +1,27 @@
 package html
 
 import (
+	"forum/internal/presentation/html/auth"
 	"forum/internal/presentation/html/home"
+	"forum/internal/presentation/html/post"
 	"forum/internal/service"
+	"html/template"
 )
 
 type TemplateHandlers struct {
-	Home *home.HomeHandler
+	Home       *home.HomeHandler
+	Login      *auth.LoginHandler
+	Register   *auth.RegisterHandler
+	Post       *post.PostHandler
+	CreatePost *post.CreateHandler
 }
 
-func NewTemplateHandlers(services *service.Service) *TemplateHandlers {
+func NewTemplateHandlers(tmpl *template.Template, services *service.Service) *TemplateHandlers {
 	return &TemplateHandlers{
-		Home: home.NewHomeHandler(services.Post, services.User, services.Category),
+		Home:       home.NewHomeHandler(tmpl, services.Post, services.User, services.Category),
+		Login:      auth.NewLoginHandler(tmpl, services.User),
+		Register:   auth.NewRegisterHandler(tmpl, services.User),
+		Post:       post.NewPostHandler(tmpl, services.User, services.Post, services.Comment),
+		CreatePost: post.NewCreateHandler(tmpl, services.User, services.Post),
 	}
 }
