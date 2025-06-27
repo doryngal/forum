dda4e256c24d10495d8507dd8cb2c378b6dd6c18
doryngal <141703428+doryngal@users.noreply.github.com>
2025-06-28 01:00:02 +0500

    feat(router): add profile page handler and update service injection in template handlers

--- backend/internal/presentation/html/handlers.go
+++ backend/internal/presentation/html/handlers.go
@@ -4,24 +4,31 @@ import (
 	"forum/internal/presentation/html/auth"
 	"forum/internal/presentation/html/home"
 	"forum/internal/presentation/html/post"
+	profile "forum/internal/presentation/html/user"
 	"forum/internal/service"
 	"html/template"
 )
 
 type TemplateHandlers struct {
-	Home       *home.HomeHandler
-	Login      *auth.LoginHandler
-	Register   *auth.RegisterHandler
+	Home     *home.HomeHandler
+	Login    *auth.LoginHandler
+	Register *auth.RegisterHandler
+
 	Post       *post.PostHandler
 	CreatePost *post.CreateHandler
+
+	Profile *profile.ProfileHandler
 }
 
 func NewTemplateHandlers(tmpl *template.Template, services *service.Service) *TemplateHandlers {
 	return &TemplateHandlers{
-		Home:       home.NewHomeHandler(tmpl, services.Post, services.User, services.Category),
-		Login:      auth.NewLoginHandler(tmpl, services.User),
-		Register:   auth.NewRegisterHandler(tmpl, services.User),
+		Home:     home.NewHomeHandler(tmpl, services.Post, services.User, services.Category),
+		Login:    auth.NewLoginHandler(tmpl, services.User, services.Session),
+		Register: auth.NewRegisterHandler(tmpl, services.User),
+
 		Post:       post.NewPostHandler(tmpl, services.User, services.Post, services.Comment),
 		CreatePost: post.NewCreateHandler(tmpl, services.User, services.Post),
+
+		Profile: profile.NewProfileHandler(tmpl, services.User, services.Post),
 	}
 }
