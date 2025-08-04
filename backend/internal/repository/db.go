0dce4bae8a7519df972f79c7cd707d3ce98d9ccc
doryngal <141703428+doryngal@users.noreply.github.com>
2025-08-04 17:24:25 +0500

    refactor(repository): update imports to use unified interfaces and decoupled sqlite implementations
    Switched to importing interfaces from root `category`, `comment`, `post`, `session`, and `user` packages
    and concrete implementations from their respective `sqlite` subpackages.
    Enhances modularity and aligns with clean architecture principles.

--- backend/internal/repository/db.go
+++ backend/internal/repository/db.go
@@ -2,11 +2,16 @@ package repository
 
 import (
 	"database/sql"
-	category_repo "forum/internal/repository/sqlite/category"
-	comment_repo "forum/internal/repository/sqlite/comment"
-	post_repo "forum/internal/repository/sqlite/post"
-	session_repo "forum/internal/repository/sqlite/session"
-	user_repo "forum/internal/repository/sqlite/user"
+	category_repo "forum/internal/repository/category"
+	category_sqlite "forum/internal/repository/category/sqlite"
+	comment_repo "forum/internal/repository/comment"
+	comment_sqlite "forum/internal/repository/comment/sqlite"
+	post_repo "forum/internal/repository/post"
+	post_sqlite "forum/internal/repository/post/sqlite"
+	session_repo "forum/internal/repository/session"
+	session_sqlite "forum/internal/repository/session/sqlite"
+	user_repo "forum/internal/repository/user"
+	user_sqlite "forum/internal/repository/user/sqlite"
 )
 
 type Repositories struct {
@@ -19,10 +24,10 @@ type Repositories struct {
 
 func NewRepositories(db *sql.DB) *Repositories {
 	return &Repositories{
-		Category: category_repo.New(db),
-		Comment:  comment_repo.New(db),
-		Session:  session_repo.New(db),
-		Post:     post_repo.New(db),
-		User:     user_repo.New(db),
+		Category: category_sqlite.New(db),
+		Comment:  comment_sqlite.New(db),
+		Session:  session_sqlite.New(db),
+		Post:     post_sqlite.New(db),
+		User:     user_sqlite.New(db),
 	}
 }
