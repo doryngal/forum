853f1a32caf767c46b5962c5ddf0e5e53de4dfcf
doryngal <141703428+doryngal@users.noreply.github.com>
2025-08-06 15:54:32 +0500

    refactor(errorhandler): improve error handling and integrate logging
    - Added `logger.Logger` to `Handler` for structured error logging
    - Replaced direct template execution with buffered rendering (`bytes.Buffer`)
    - Logs failures in template rendering or writing to response
    - Updated `HandleError` to accept `errLog error` for contextual logging

--- backend/internal/presentation/html/errorhandler/error.go
+++ backend/internal/presentation/html/errorhandler/error.go
@@ -1,22 +1,26 @@
 package errorhandler
 
 import (
+	"bytes"
+	"forum/pkg/logger"
 	"html/template"
 	"net/http"
 )
 
 type Handler struct {
-	tmpl *template.Template
+	tmpl   *template.Template
+	logger logger.Logger
 }
 
-func NewErrorHandler(tmpl *template.Template) *Handler {
+func NewErrorHandler(tmpl *template.Template, logger logger.Logger) *Handler {
 	return &Handler{
-		tmpl: tmpl,
+		tmpl:   tmpl,
+		logger: logger,
 	}
 }
 
-func (h *Handler) HandleError(w http.ResponseWriter, errorMsg string, statusCode int) {
-	w.WriteHeader(statusCode)
+func (h *Handler) HandleError(w http.ResponseWriter, errorMsg string, errLog error, statusCode int) {
+	var buf bytes.Buffer
 
 	data := struct {
 		ErrorCode int
@@ -26,7 +30,14 @@ func (h *Handler) HandleError(w http.ResponseWriter, errorMsg string, statusCode
 		ErrorMsg:  errorMsg,
 	}
 
-	if err := h.tmpl.ExecuteTemplate(w, "error.html", data); err != nil {
+	if err := h.tmpl.ExecuteTemplate(&buf, "error.html", data); err != nil {
+		h.logger.Errorf("failed to render error page: %v", err)
 		http.Error(w, "Error rendering error page", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(statusCode)
+	if _, err := buf.WriteTo(w); err != nil {
+		h.logger.Errorf("failed to write response: %v", err)
 	}
 }
